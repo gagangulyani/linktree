786a377b3cae0a8ce0f4921e7619fb0c57b6fa51
Gagan Deep Singh <gagangulyanig@gmail.com>
2024-06-07 16:15:43 +0530

    Added LinkedIn link

--- src/util/getLinks.ts
+++ src/util/getLinks.ts
@@ -9,14 +9,19 @@ export default function getLinks(): {
   return {
     urls: [
       {
-        name: "Instagram",
-        link: "https://instagram.com/gagan_gulyani",
-        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png",
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/in/gagan-gulyani",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/640px-LinkedIn_logo_initials.png",
       },
       {
         name: "Twitter",
         link: "https://twitter.com/GaganGulyani",
         logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/2491px-Logo_of_Twitter.svg.png",
+      },
+      {
+        name: "Instagram",
+        link: "https://instagram.com/gagan_gulyani",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png",
       },
       {
         name: "GitHub",
